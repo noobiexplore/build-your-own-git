968c859114e960b859b150575d2c919eb45cd561
noobiexplorer <noobiexplorer@example.com>
2024-05-28 23:56:54 +0600

    trial: pass 2nd stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,11 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
-
-    // Uncomment this block to pass the first stage
-
     if (argc < 2) {
         std::cerr << "No command provided.\n";
         return EXIT_FAILURE;
